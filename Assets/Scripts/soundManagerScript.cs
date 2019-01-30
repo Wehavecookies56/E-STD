24a6219945b1c8101638e16e43bfbfb914c294a8
sebastian mol <sebastianmol1996@gamil.com>
2019-01-30 12:38:34 +0000

    cleanUp

--- Assets/Scripts/soundManagerScript.cs
+++ Assets/Scripts/soundManagerScript.cs
@@ -31,28 +31,21 @@ public class soundManagerScript : MonoBehaviour
     private void Start()
     {
         startLoop(backgroundSounds.HORRORLOOP, gameObject.transform);
-        playOnce(enviromentSounds.THUNDER2, gameObject.transform);
-        
+     
     }
 
     public void playOnce(enviromentSounds sound, Transform position)
     {
-       
-               GameObject instance =  Instantiate(enviromentAudio[(int)sound]);
-                instance.transform.position = transform.position;
-                instance = null;
-       
+        GameObject instance = Instantiate(enviromentAudio[(int)sound]);
+        instance.transform.position = transform.position;
+        instance = null;
     }
 
     public void startLoop(backgroundSounds sound, Transform parent)
     {
-        switch(sound)
-        {
-            case backgroundSounds.HORRORLOOP:
-                GameObject instance = Instantiate(backroundSounds[(int)sound]);
-                instance.transform.SetParent(parent);
-                break;
-        }
+        GameObject instance = Instantiate(backroundSounds[(int)sound]);
+        instance.transform.SetParent(parent);
+
     }
 
 }
