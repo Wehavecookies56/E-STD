cbf4c3b924c02550cde7fa74b4f580757e7b6658
sebastian mol <sebastianmol1996@gamil.com>
2019-01-30 12:31:58 +0000

    audiomanager started
    created thunder, door open, ambient sound for the game.

---
+++ Assets/Scripts/soundManagerScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundManagerScript : MonoBehaviour
+{
+    public static soundManagerScript audioPlayer { get; private set; }
+
+    public enum characterSounds { };
+    public GameObject[] characterAudio;
+
+    public enum enviromentSounds { DOOROPEN, THUNDER1, THUNDER2};
+    public GameObject[] enviromentAudio;
+
+    public enum backgroundSounds { HORRORLOOP };
+    public GameObject[] backroundSounds;
+
+    private void Awake()
+    {
+        if (audioPlayer == null)
+        {
+            audioPlayer = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        startLoop(backgroundSounds.HORRORLOOP, gameObject.transform);
+        playOnce(enviromentSounds.THUNDER2, gameObject.transform);
+        
+    }
+
+    public void playOnce(enviromentSounds sound, Transform position)
+    {
+       
+               GameObject instance =  Instantiate(enviromentAudio[(int)sound]);
+                instance.transform.position = transform.position;
+                instance = null;
+       
+    }
+
+    public void startLoop(backgroundSounds sound, Transform parent)
+    {
+        switch(sound)
+        {
+            case backgroundSounds.HORRORLOOP:
+                GameObject instance = Instantiate(backroundSounds[(int)sound]);
+                instance.transform.SetParent(parent);
+                break;
+        }
+    }
+
+}
