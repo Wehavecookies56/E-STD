9ab6fbbd335f60aabe4ef809cbea6c7c401ef616
Unknown <d.sheshtanov@gmail.com>
2019-02-06 15:32:28 +0000

    Subfolders & Weather Scene
    Created subfolders for main folders, also new weather testing scene.

--- Assets/Scripts/UI/spinHeart.cs
+++ Assets/Scripts/UI/spinHeart.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class spin : MonoBehaviour
+public class spinHeart : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
