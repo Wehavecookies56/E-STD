d494978985e6fa1571dfc6d7f21795d067c44013
Unknown <d.sheshtanov@gmail.com>
2019-02-07 18:57:50 +0000

    Weather System
    Added new textures and materials for weather effects and 3 new particle prefabs for clouds, rain and lightning as well as a prefab for all 3 effects. Also a script for playing thunder sounds (not complete) and a test scene for weather.

--- Assets/Scripts/NPC/Pathfinding/PathNode.cs
+++ Assets/Scripts/NPC/Pathfinding/PathNode.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿//Script written by Dan Sheshtanov
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
---
+++ Assets/Scripts/ThunderPlayer.cs
@@ -0,0 +1,42 @@
+﻿//Script written by Dan Sheshtanov
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThunderPlayer : MonoBehaviour
+{
+    //particle system component reference
+    ParticleSystem ps;
+    //array to hold lightning particles, there can only ever be 1 at a time
+    ParticleSystem.Particle[] particles = new ParticleSystem.Particle[1];
+    //controls audio call to be once per particle
+    bool needReset = false;
+    
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        int numOfParticles = ps.GetParticles(particles);
+
+        if (!needReset) //if particle hasn't already been detected
+        {
+
+            if (numOfParticles > 0)
+            {
+                Debug.Log("Triggered");
+                needReset = true;
+            }
+        }
+        else
+        {
+            if(numOfParticles < 1)
+            {
+                needReset = false;
+            }
+        }
+        
+    }
+}
