9471ce933d4fa931fdba59735db50b69ee16adae
sebastian mol <sebastianmol1996@gamil.com>
2019-02-13 17:09:24 +0000

    stats started and all item descriptions done and ready to be filled

--- Assets/Scripts/inventoryButton.cs
+++ Assets/Scripts/inventoryButton.cs
@@ -4,21 +4,5 @@ using UnityEngine;
 
 public class inventoryButton : MonoBehaviour
 {
-    public GameObject description;
-
-    private void Start()
-    {
-        description = GameObject.FindGameObjectWithTag("UIkey");
-    }
-
-    private void OnMouseOver()
-    {
-        description.SetActive(true);
-        Debug.Log("mouse over");
-    }
-
-    private void OnMouseExit()
-    {
-        description.SetActive(false);
-    }
+    
 }
