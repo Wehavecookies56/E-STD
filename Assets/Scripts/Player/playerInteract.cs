c874e1e7c4e6abd056789e690156c71cd2653ecb
Toby Wishart <qwenit56@gmail.com>
2019-02-27 21:43:37 +0000

    Fixed interacting through walls and interaction distance

--- Assets/Scripts/Player/playerInteract.cs
+++ Assets/Scripts/Player/playerInteract.cs
@@ -29,13 +29,20 @@ public class playerInteract : MonoBehaviour {
         //Raycast from camera
         var r = new Ray(playerLookCamera.position, playerLookCamera.forward);
         //Raycast only for objects with items layer
-        if (Physics.Raycast(r, out rh, distance, 1 << LayerMask.NameToLayer("items"))) {
-            GameObject lookedAt = rh.transform.gameObject;
-            lastLookedAt = lookedAt;
-            //Boolean_446AB9C2 is the id for the "enabled" property in the item highlight shader
-            lookedAt.GetComponent<MeshRenderer>().material.SetFloat("Boolean_446AB9C2", 1);
-            if (Input.GetButtonDown("Fire1")) {
-                onInteract.Invoke(lookedAt);
+        if (Physics.Raycast(r, out rh, distance)) {
+            if (1 << rh.transform.gameObject.layer == 1 << LayerMask.NameToLayer("items")) {
+                GameObject lookedAt = rh.transform.gameObject;
+                lastLookedAt = lookedAt;
+                //Boolean_446AB9C2 is the id for the "enabled" property in the item highlight shader
+                lookedAt.GetComponent<MeshRenderer>().material.SetFloat("Boolean_446AB9C2", 1);
+                if (Input.GetButtonDown("Fire1")) {
+                    onInteract.Invoke(lookedAt);
+                }
+            } else {
+                if (lastLookedAt != null) {
+                    lastLookedAt.GetComponent<MeshRenderer>().material.SetFloat("Boolean_446AB9C2", 0);
+                    lastLookedAt = null;
+                }
             }
         } else {
             if (lastLookedAt != null) {
