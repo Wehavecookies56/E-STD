a044f4137e8f5e3d5bff71e364b13a773150d0fa
Unknown <d.sheshtanov@gmail.com>
2019-02-28 17:04:21 +0000

    First floor lighting complete and collision data setup

---
+++ Assets/Scripts/LightPlayerDetector.cs
@@ -0,0 +1,25 @@
+﻿//Script Written By Dan Sheshtanov
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightPlayerDetector : MonoBehaviour
+{
+    private Light l;
+
+    private void Start()
+    {
+        l = GetComponent<Light>();
+        l.enabled = false;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        l.enabled = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        l.enabled = false;
+    }
+}
